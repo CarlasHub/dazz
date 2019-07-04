a5f5fe784d1d31cf6c802254a38777b0e441d7c5
carlashub <carlawlondon@gmail.com>
2019-07-04 10:44:14 +0100

    First commit

---
+++ inc/social-icons.php
@@ -0,0 +1,14 @@
+<div class="wrapper-social">
+    <ul>
+        <li class="facebook ">
+            <i class="fab fa-facebook " aria-hidden="true"></i>
+        </li>
+        <li class="twitter ">
+            <i class="fab fa-twitter hover" aria-hidden="true"></i>
+        </li>
+        <li class="instagram ">
+            <i class="fab fa-instagram" aria-hidden="true"></i>
+        </li>
+        <li><i class="fa fa-twitter fa-2x"></i></li>
+    </ul>
+</div>
